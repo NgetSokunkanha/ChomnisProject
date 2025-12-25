8bb1d8307a278819203228160931c73297c8bccb
NgetSokunkanha <ngetsokunkanha1@gmail.com>
2025-12-25 11:35:18 +0700

    add more pages

---
+++ 3booknow.js
@@ -0,0 +1,8 @@
+
+const confirmBtn = document.getElementById("confirmBooking");
+
+confirmBtn.addEventListener("click", function () {
+    alert("✅ Booking successful! Your booking has been confirmed.");
+    window.location.href =  "1landingPage.html";
+});
+
